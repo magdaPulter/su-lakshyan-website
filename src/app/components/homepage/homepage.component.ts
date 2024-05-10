209e89ab54ef94485f8f440400a98dd240069f0b
magdaPulter <magda.pulter@gmail.com>
2024-05-10 10:55:01 +0200

    homepage section extracted

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { ReadMoreBtnComponent } from '../read-more-btn/read-more-btn.component';
+import { HomeAboutComponent } from '../home-about/home-about.component';
+import { HomeCreateComponent } from '../home-create/home-create.component';
+import { HomeMottoComponent } from '../home-motto/home-motto.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
-  imports: [ReadMoreBtnComponent],
+  imports: [
+    ReadMoreBtnComponent,
+    HomeAboutComponent,
+    HomeCreateComponent,
+    HomeMottoComponent,
+  ],
 })
 export class HomepageComponent {}
