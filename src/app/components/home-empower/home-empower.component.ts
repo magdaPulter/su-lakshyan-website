591a04ca18f26c819d8b9848ddfe5c70e3e62022
magdaPulter <magda.pulter@gmail.com>
2024-05-23 16:33:53 +0200

    home-empower component added

---
+++ src/app/components/home-empower/home-empower.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-empower',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-empower.component.html',
+  styleUrl: './home-empower.component.scss'
+})
+export class HomeEmpowerComponent {
+
+}
