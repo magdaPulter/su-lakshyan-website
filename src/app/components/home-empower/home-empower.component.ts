0d4178c40854f74d95595ed9e36193849846d737
magdaPulter <magda.pulter@gmail.com>
2024-05-27 21:00:19 +0200

    photos added

--- src/app/components/home-empower/home-empower.component.ts
+++ src/app/components/home-empower/home-empower.component.ts
@@ -5,8 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './home-empower.component.html',
-  styleUrl: './home-empower.component.scss'
+  styleUrl: './home-empower.component.scss',
 })
-export class HomeEmpowerComponent {
-
-}
+export class HomeEmpowerComponent {}
