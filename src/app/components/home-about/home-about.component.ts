d5aa84d361ef02bfafa756c7bd0f1e2d4ca7675e
magdaPulter <magda.pulter@gmail.com>
2024-06-05 22:00:59 +0200

    mobile view for hompage implemented

--- src/app/components/home-about/home-about.component.ts
+++ src/app/components/home-about/home-about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ReadMoreBtnComponent } from '../read-more-btn/read-more-btn.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-about',
   standalone: true,
   templateUrl: './home-about.component.html',
   styleUrl: './home-about.component.scss',
-  imports: [ReadMoreBtnComponent],
+  imports: [ReadMoreBtnComponent, RouterLink],
 })
 export class HomeAboutComponent {}
