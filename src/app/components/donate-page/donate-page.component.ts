b96a66e1570f545a5135bc959d421264606f14c6
magdaPulter <magda.pulter@gmail.com>
2024-06-14 10:46:26 +0200

    donate-page added

---
+++ src/app/components/donate-page/donate-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donate-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './donate-page.component.html',
+  styleUrl: './donate-page.component.scss',
+})
+export class DonatePageComponent {}
