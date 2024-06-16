3ab388b5c517305bcca8bac36fef09e10d0bcea1
magdaPulter <magda.pulter@gmail.com>
2024-06-16 18:25:41 +0200

    hamburger menu added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, WritableSignal, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isMenuOpen: WritableSignal<boolean> = signal(false);
+  ngOnInit() {
+    this.isMenuOpen.set(false);
+  }
+
+  close: string = '../../../assets/close.svg';
+  open: string = '../../../assets/menu.svg';
+
+  toggleMenu(): void {
+    this.isMenuOpen.update((isMenuOpen) => !isMenuOpen);
+  }
+}
