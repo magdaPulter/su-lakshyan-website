9711f61e077e31b4fc5dcfedf58f5e9d284a1070
magdaPulter <magda.pulter@gmail.com>
2024-06-18 19:36:30 +0200

    contact-us-page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomepageComponent } from './components/homepage/homepage.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
 import { ProjectPageComponent } from './components/project-page/project-page.component';
 import { DonatePageComponent } from './components/donate-page/donate-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'about', component: AboutPageComponent },
   { path: 'projects', component: ProjectPageComponent },
   { path: 'donate', component: DonatePageComponent },
+  { path: 'contact', component: ContactPageComponent },
 ];
