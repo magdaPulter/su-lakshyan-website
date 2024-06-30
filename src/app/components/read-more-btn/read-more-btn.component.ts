05f3210ebc4398915066250e28fcbec2d02397f2
magdaPulter <magda.pulter@gmail.com>
2024-06-30 19:15:25 +0200

    button border color fixed

--- src/app/components/read-more-btn/read-more-btn.component.ts
+++ src/app/components/read-more-btn/read-more-btn.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-read-more-btn',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './read-more-btn.component.html',
   styleUrl: './read-more-btn.component.scss',
 })
-export class ReadMoreBtnComponent {}
+export class ReadMoreBtnComponent {
+  @Input() isAboutPage!: boolean;
+}
