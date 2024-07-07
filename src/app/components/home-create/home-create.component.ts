4fc3ee4b7a47734e8366372cac528b18380f2653
magdaPulter <magda.pulter@gmail.com>
2024-07-07 15:23:21 +0200

    homepage tablet-view added

--- src/app/components/home-create/home-create.component.ts
+++ src/app/components/home-create/home-create.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { ReadMoreBtnComponent } from '../read-more-btn/read-more-btn.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-create',
   standalone: true,
-  imports: [],
   templateUrl: './home-create.component.html',
-  styleUrl: './home-create.component.scss'
+  styleUrl: './home-create.component.scss',
+  imports: [ReadMoreBtnComponent, RouterLink],
 })
-export class HomeCreateComponent {
-
-}
+export class HomeCreateComponent {}
